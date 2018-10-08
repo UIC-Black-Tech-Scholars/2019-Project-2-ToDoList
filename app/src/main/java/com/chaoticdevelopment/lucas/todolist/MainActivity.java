82341f7b5f772aeb8f20196158aceb39e5244b0f
lucasoskorep <lucas.oskorep@gmail.com>
2018-10-08 15:46:16 -0500

    Adding initial code, removing older codebases

---
+++ app/src/main/java/com/chaoticdevelopment/lucas/todolist/MainActivity.java
@@ -0,0 +1,88 @@
+package com.chaoticdevelopment.lucas.todolist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+//    static ArrayList<String> items = new ArrayList<>();
+//    ArrayAdapter<String> itemsAdapter;
+//    ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        lvItems = findViewById(R.id.lvItems);
+//        itemsAdapter = new ArrayAdapter<>(this,
+//                android.R.layout.simple_list_item_1, items);
+//        lvItems.setAdapter(itemsAdapter);
+//        readItems();
+
+//        setupListViewListener();
+    }
+
+//    private void setupListViewListener() {
+//        lvItems.setOnItemLongClickListener(
+//                new AdapterView.OnItemLongClickListener() {
+//                    @Override
+//                    public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
+//                        items.remove(pos);
+//                        itemsAdapter.notifyDataSetChanged();
+////                        writeItems();
+//                        return true;
+//                    }
+//                });
+//    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+//    public void onAddItem(View V) {
+//        EditText etNewItem = findViewById(R.id.etNewItem);
+//        String itemText = etNewItem.getText().toString();
+//        items.add(itemText);
+//        itemsAdapter.notifyDataSetChanged();
+//        etNewItem.setText("");
+////        writeItems();
+//    }
+
+
+//    private void readItems() {
+//        File filesDir = getFilesDir();
+//        File todoFile = new File(filesDir, "todo.txt");
+//
+//        Log.e("FILE", todoFile.toString());
+//        try {
+//            items.removeAll(items);
+//            items.addAll(FileUtils.readLines(todoFile));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+//    private void writeItems() {
+//        File filesDir = getFilesDir();
+//        File todoFile = new File(filesDir, "todo.txt");
+//        try {
+//            FileUtils.writeLines(todoFile, items);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
+
