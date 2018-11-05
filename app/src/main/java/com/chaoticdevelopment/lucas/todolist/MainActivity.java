ac6e739423bf051c105dca2dac7b507bcfe2436e
lucasoskorep <lucas.oskorep@gmail.com>
2018-11-05 14:25:58 -0600

    Finished the project.

--- app/src/main/java/com/chaoticdevelopment/lucas/todolist/MainActivity.java
+++ app/src/main/java/com/chaoticdevelopment/lucas/todolist/MainActivity.java
@@ -2,40 +2,54 @@ package com.chaoticdevelopment.lucas.todolist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
-//    static ArrayList<String> items = new ArrayList<>();
-//    ArrayAdapter<String> itemsAdapter;
-//    ListView lvItems;
+    static ArrayList<String> items = new ArrayList<>();
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        lvItems = findViewById(R.id.lvItems);
-//        itemsAdapter = new ArrayAdapter<>(this,
-//                android.R.layout.simple_list_item_1, items);
-//        lvItems.setAdapter(itemsAdapter);
-//        readItems();
+        lvItems = findViewById(R.id.lvItems);
+        itemsAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("Item 1");
+        items.add("Item 2");
+        readItems();
 
-//        setupListViewListener();
+        setupListViewListener();
     }
 
-//    private void setupListViewListener() {
-//        lvItems.setOnItemLongClickListener(
-//                new AdapterView.OnItemLongClickListener() {
-//                    @Override
-//                    public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
-//                        items.remove(pos);
-//                        itemsAdapter.notifyDataSetChanged();
-////                        writeItems();
-//                        return true;
-//                    }
-//                });
-//    }
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
+                        items.remove(pos);
+                        itemsAdapter.notifyDataSetChanged();
+                        writeItems();
+                        return true;
+                    }
+                });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -52,37 +66,37 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-//    public void onAddItem(View V) {
-//        EditText etNewItem = findViewById(R.id.etNewItem);
-//        String itemText = etNewItem.getText().toString();
-//        items.add(itemText);
-//        itemsAdapter.notifyDataSetChanged();
-//        etNewItem.setText("");
-////        writeItems();
-//    }
+    public void onAddItem(View V) {
+        EditText etNewItem = findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        items.add(itemText);
+        itemsAdapter.notifyDataSetChanged();
+        etNewItem.setText("");
+        writeItems();
+    }
 
 
-//    private void readItems() {
-//        File filesDir = getFilesDir();
-//        File todoFile = new File(filesDir, "todo.txt");
-//
-//        Log.e("FILE", todoFile.toString());
-//        try {
-//            items.removeAll(items);
-//            items.addAll(FileUtils.readLines(todoFile));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
 
-//    private void writeItems() {
-//        File filesDir = getFilesDir();
-//        File todoFile = new File(filesDir, "todo.txt");
-//        try {
-//            FileUtils.writeLines(todoFile, items);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+        Log.e("FILE", todoFile.toString());
+        try {
+            items.removeAll(items);
+            items.addAll(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
